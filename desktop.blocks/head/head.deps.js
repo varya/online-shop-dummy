587f6d46d47847869985d3f02973041f6d4cf728
Varvara Stepanova <toivonen@toivonen.ru>
2012-11-28 00:06:45 +0400

    Head layout

---
+++ desktop.blocks/head/head.deps.js
@@ -0,0 +1,12 @@
+({
+    shouldDeps: [
+        {
+            block: 'b-layout-table',
+            mods: { 'layout' : '70-30' }
+        },
+        {
+            block: 'b-search',
+            elems: ['layout']
+        }
+    ]
+})
