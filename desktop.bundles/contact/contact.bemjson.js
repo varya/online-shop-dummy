24d768262954d9a55e95a65de37a8c74c60b0e0d
Varvara Stepanova <toivonen@toivonen.ru>
2012-11-28 22:53:19 +0400

    Contact page

---
+++ desktop.bundles/contact/contact.bemjson.js
@@ -0,0 +1,16 @@
+({
+    block: 'b-page',
+    title: 'contact',
+    head: [
+        { elem: 'css', url: '_contact.css', ie: false},
+        { elem: 'css', url: '_contact.ie.css', ie: true },
+        { block: 'i-jquery', elem: 'core'},
+        { elem: 'js', url:'_contact.js'},
+    ],
+    content: [
+        {
+            block: 'head'
+        },
+        'Put page content here...'
+    ]
+})
